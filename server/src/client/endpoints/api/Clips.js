d9734f568e0216ca85c38676f9dd765d40fc76ba
Navy.gif <navydotgif@gmail.com>
2022-03-24 16:53:54 +0200

    testing video chunking

--- server/src/client/endpoints/api/Clips.js
+++ server/src/client/endpoints/api/Clips.js
@@ -36,12 +36,31 @@ class Clips extends APIEndpoint {
 
     async clip(req, res) {
 
-        const { params: { clip } } = req;
+        const { params: { clip }, headers: { range } } = req;
         const files = fs.readdirSync(path.join(this.client.baseDirectory, 'media'));
         if (!files.some((file) => file === clip)) return res.status(404).send('Not found');
-        res.status(200).sendFile(clip, {
-            root: this.mediaDir
-        });
+
+        const videoPath = path.join(this.mediaDir, clip);
+        const { size } = fs.statSync(videoPath);
+        const chunkSize = 1024 * 1024 * 5;
+        const start = parseInt(range.split('=')[1].split('-')[0]);
+        const end = Math.min(start + chunkSize, size - 1);
+        const length = end - start + 1;
+
+        const headers = {
+            'Content-Range': `bytes ${start}-${end}/${size}`,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': length,
+            'Content-Type': 'video/mp4'
+        };
+
+        const stream = fs.createReadStream(videoPath, { start, end });
+        res.status(206).set(headers);
+        stream.pipe(res);
+
+        // res.status(200).sendFile(clip, {
+        //     root: this.mediaDir
+        // });
 
     }
 
