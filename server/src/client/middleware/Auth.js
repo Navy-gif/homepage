7e8ac200b20973f0ce5129301adfb2ba1677cea4
Navy.gif <navydotgif@gmail.com>
2022-03-26 14:16:02 +0200

    token auth

--- server/src/client/middleware/Auth.js
+++ server/src/client/middleware/Auth.js
@@ -1,7 +1,32 @@
-const CheckAuth = (req, res, next) => {
+const msg = { message: 'Must be authenticated for this endpoint' };
+
+const CheckSession = (req, res, next) => {
     res.set('Access-Control-Allow-Credentials', true);
     if (req.isAuthenticated()) return next();
-    return res.status(401).json({ message: 'Must be authenticated for this endpoint' });
+    return res.status(401).json(msg);
 };
 
-module.exports = CheckAuth;
+const CheckSessionOrToken = async (req, res, next) => {
+
+    res.set('Access-Control-Allow-Credentials', true);
+    if (req.isAuthenticated()) return next();
+
+    const token = req.get('Authorization') || null;
+    const user = token ? await req.client.users.checkToken(token) : null;
+    if (user) {
+        req.user = user;
+        return next();
+    }
+
+    return res.status(401).json(msg);
+
+};
+
+const CheckToken = async (req, res, next) => {
+    const token = req.get('Authorization') || null;
+    const result = token ? await req.client.users.checkToken(token) : false;
+    if (result) return next();
+    return res.status(401).json(msg);
+};
+
+module.exports = { CheckSession, CheckSessionOrToken, CheckToken };
