1b103c41a07c4ba34d2267c110af3ccb603ae219
Navy.gif <navydotgif@gmail.com>
2022-03-26 14:16:54 +0200

    token generation

--- server/src/client/endpoints/api/User.js
+++ server/src/client/endpoints/api/User.js
@@ -1,5 +1,5 @@
 const { APIEndpoint } = require('../../interfaces');
-const { CheckAuth } = require('../../middleware');
+const { CheckSession, Permissions } = require('../../middleware');
 
 class User extends APIEndpoint {
 
@@ -15,7 +15,11 @@ class User extends APIEndpoint {
             ['get', this.get.bind(this)]
         ];
 
-        this.middleware = [CheckAuth];
+        this.subpaths = [
+            ['/token', 'get', this.token.bind(this), [Permissions('upload')]]
+        ];
+
+        this.middleware = [CheckSession];
 
         this.init();
 
@@ -25,6 +29,15 @@ class User extends APIEndpoint {
         // this.logger.debug(`Get user? ${req.user}`);
         if (req.user) res.status(200).json(req.user);
         else res.status(401).end();
+    }
+
+    async token(req, res) {
+
+        const { user } = req;
+        this.logger.info(`Generating upload token for ${user.tag}`);
+        const token = await this.client.users.generateToken(user, ['upload']);
+        res.status(200).json({ token });
+
     }
 
 }
